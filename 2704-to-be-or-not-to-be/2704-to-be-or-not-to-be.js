1d27bb29e87f7091a267f57b08db9d9be62c0496
Utkarsh (Gaurav) <dubeutkarsh7@gmail.com>
2026-06-28 23:06:13 +0530

    Time: 43 ms (60.76%), Space: 54.4 MB (10.4%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(val2) {
+            if (val === val2) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: function(val2) {
+            if (val !== val2) return true;
+            throw new Error("Equal");
+        }
+    };
+};
