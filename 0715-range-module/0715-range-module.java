1875253955abde8e4d416c9e499b9b7f6148df10
Utkarsh (Gaurav) <dubeutkarsh7@gmail.com>
2026-06-29 08:41:54 +0200

    Time: 44 ms (58.33%), Space: 57.1 MB (58.82%) - LeetHub

---
+++ 0715-range-module/0715-range-module.java
@@ -0,0 +1,70 @@
+class RangeModule {
+
+    private TreeMap<Integer, Integer> map;
+
+    public RangeModule() {
+        map = new TreeMap<>();
+    }
+
+    public void addRange(int left, int right) {
+        if (left >= right) return;
+
+        Integer start = map.floorKey(left);
+
+        if (start != null && map.get(start) >= left) {
+            left = start;
+            right = Math.max(right, map.get(start));
+            map.remove(start);
+        }
+
+        Integer next = map.ceilingKey(left);
+
+        while (next != null && next <= right) {
+            right = Math.max(right, map.get(next));
+            map.remove(next);
+            next = map.ceilingKey(left);
+        }
+
+        map.put(left, right);
+    }
+
+    public boolean queryRange(int left, int right) {
+        Integer start = map.floorKey(left);
+
+        return start != null && map.get(start) >= right;
+    }
+
+    public void removeRange(int left, int right) {
+        if (left >= right) return;
+
+        Integer start = map.floorKey(left);
+
+        if (start != null && map.get(start) > left) {
+            int end = map.get(start);
+
+            if (start < left)
+                map.put(start, left);
+
+            if (end > right)
+                map.put(right, end);
+
+            map.remove(start);
+            if (start < left)
+                map.put(start, left);
+        }
+
+        Integer next = map.ceilingKey(left);
+
+        while (next != null && next < right) {
+            int end = map.get(next);
+            map.remove(next);
+
+            if (end > right) {
+                map.put(right, end);
+                break;
+            }
+
+            next = map.ceilingKey(left);
+        }
+    }
+}
